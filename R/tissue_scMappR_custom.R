3f84de8c8ce488e078a5cc988d41928fe769ea4f
DustinSokolowski <dustin.sokolowski@sickkids.ca>
2019-12-18 17:35:57 -0500

    example bug fix

--- R/tissue_scMappR_custom.R
+++ R/tissue_scMappR_custom.R
@@ -43,7 +43,7 @@
 #' POA_Rank_signature <- POA_example$POA_Rank_signature
 #' sig <- get_gene_symbol(POA_Rank_signature)
 #' Signature <- POA_Rank_signature
-#' rownames(Signature) <- sig(rowname)
+#' rownames(Signature) <- sig$rowname
 #' genes <- rownames(Signature)[1:60]
 #' heatmap_test <- tissue_scMappR_custom( genes, signature_matrix = Signature,
 #'                                       output_directory =  "scMappR_test", toSave = FALSE)
