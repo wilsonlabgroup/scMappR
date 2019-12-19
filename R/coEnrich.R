4b42ed72930f06dfeb850e875e3659772b05a984
DustinSokolowski <dustin.sokolowski@sickkids.ca>
2019-12-18 21:15:45 -0500

    add examples

--- R/coEnrich.R
+++ R/coEnrich.R
@@ -39,7 +39,9 @@
 #' POA_generes <- POA_example$POA_generes
 #' POA_OR_signature <- POA_example$POA_OR_signature
 #' POA_Rank_signature <- POA_example$POA_Rank_signature
+#' sig <- get_gene_symbol(POA_Rank_signature)
 #' Signature <- POA_Rank_signature
+#' rownames(Signature) <- sig$rowname
 #' genes <- rownames(Signature)[1:60]
 #' heatmap_test <- tissue_scMappR_custom( genes, signature_matrix = Signature,
 #' output_directory =  "scMappR_test", toSave = FALSE)
