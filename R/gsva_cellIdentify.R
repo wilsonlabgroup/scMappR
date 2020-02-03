601c0dba07d6df67346030da1f88415a39298653
DustinSokolowski <dustin.sokolowski@sickkids.ca>
2020-02-03 14:26:18 -0500

    update description and gsva to remove notes.

--- R/gsva_cellIdentify.R
+++ R/gsva_cellIdentify.R
@@ -35,7 +35,8 @@
 #' data(sm)
 #' toProcess <- list(example = sm)
 #' tst1 <- process_from_count(toProcess, "testProcess")
-#' cellnames <- gsva_cellIdentify(tst1, theSpecies = "mouse", naming_preference = "brain", rda_path = "")
+#' cellnames <- gsva_cellIdentify(tst1, theSpecies = "mouse",
+#'  naming_preference = "brain", rda_path = "")
 #' 
 #' }
 #'  
