af0660b5cc74975d96f502bca17af008bf30145b
DustinSokolowski <dustin.sokolowski@sickkids.ca>
2020-02-05 15:18:02 -0500

    scMappR_tissues add, fix documentation "path"

---
+++ R/scMappR_tissues.R
@@ -0,0 +1,20 @@
+#' scMappR_tissues
+#' 
+#' Tissues available in scMappR as of February 2020.
+#'
+#' A vector of tissues available in scMappR as of February 2020.
+#'
+#' @rdname scMappR_tissues
+#' @name scMappR_tissues
+#' 
+#' @usage data(scMappR_tissues)
+#'
+#' @format A vector of tissue names available for tissue_scMappR_internal or to download and use in scMappR_and_pathway_analysis.
+#' \describe{
+#'   \item{scMappR_tissues}{ A list of 174 tissue names from PanglaoDB. }
+#' }
+#' @examples 
+#' data(scMappR_tissues)
+#' @keywords datasets
+#' 
+NULL
