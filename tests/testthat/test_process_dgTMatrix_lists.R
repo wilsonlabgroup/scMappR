80301135d9708dac4c7c222bfe20fbb8cd6bb080
DustinSokolowski <dustin.sokolowski@sickkids.ca>
2020-02-06 17:47:07 -0500

    Remove tests that involve downloading data

--- tests/testthat/test_process_dgTMatrix_lists.R
+++ tests/testthat/test_process_dgTMatrix_lists.R
@@ -1,16 +1,4 @@
 
-testthat::test_that('cell-type to convert to signature matrix should work', {
-  
-data(sm)
-toProcess <- list(example = sm)
-tst1 <- process_dgTMatrix_lists(toProcess, name = "testPropcess", species_name = -9,
-                                naming_preference = "eye", rda_path = "~/scMappR/data")
-
-output_list <- all(names(tst1) == c("wilcoxon_rank_mat_t", "wilcoxon_rank_mat_or", "generes"))
-testthat::expect_true(output_list)
-testthat::expect_true(is.list(tst1))
-
-})
 
 testthat::test_that('too few cells, should fail', {
   
